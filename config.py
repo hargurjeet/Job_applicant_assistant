3e0621445d98fb766ad6978f9131a25d61ddadd9
Hargurjeet Singh Ganger <gurjeet333@gmail.com>
2025-08-22 16:10:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 # Load from environment for security
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "AIzaSyA1bkUwHZ2egd5nJFFb6r9Cn0qj7GgCB_o")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "########################")
 
 # Model name
 GEMINI_MODEL_NAME = "gemini-2.5-flash"
